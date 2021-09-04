967a0c14c4448194270fa02d913c0a203d2a2820
jgreen44 <jgreen44@asu.edu>
2021-09-04 14:43:53 -0500

    initial commit

---
+++ src/main/java/com/jasongreen/springboot/basics/springbootin10steps/SpringBootIn10StepsApplication.java
@@ -0,0 +1,15 @@
+package com.jasongreen.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn10StepsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootIn10StepsApplication.class, args);
+
+    }
+
+}
